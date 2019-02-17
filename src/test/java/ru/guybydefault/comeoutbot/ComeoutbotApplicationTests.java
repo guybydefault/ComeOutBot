ad62ed29c48ad31d4c5644aa9ff7b5e0abac05cd
GuyByDefault <lexmint@rambler.ru>
2019-02-17 23:55:11 +0300

    Initial commit

---
+++ src/test/java/ru/guybydefault/comeoutbot/ComeoutbotApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.guybydefault.comeoutbot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ComeoutbotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
